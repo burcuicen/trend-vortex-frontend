a25658b243ff807a22b8ba7606069866889060cf
burcuicen <burcuicen2000@gmail.com>
2023-07-15 18:41:36 +0300

    init project

---
+++ src/pages/landing-page.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  h1 Trend Vortex
+  p Welcome to Trend Vortex, a place to find the latest trends in the world of technology.
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LandingPage',
+});
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,12 +3,11 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('src/layouts/main-layout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/landing-page.vue') },
+    ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),

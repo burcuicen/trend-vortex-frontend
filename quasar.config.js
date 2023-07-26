cad6bc8041471396b1680e1b12d2f063af59de25
burcuicen <burcuicen2000@gmail.com>
2023-07-26 23:57:24 +0300

    chore:remove i18n

--- quasar.config.js
+++ quasar.config.js
@@ -27,7 +27,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['i18n', 'axios', 'app'],
+    boot: ['axios', 'app'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.scss'],

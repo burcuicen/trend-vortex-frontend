ca85388beac0f03f5980f2018cd12c769f3c6569
burcuicen <burcuicen2000@gmail.com>
2023-07-27 18:32:54 +0300

    fix:i18n

--- quasar.config.js
+++ quasar.config.js
@@ -85,20 +85,7 @@ module.exports = configure(function (/* ctx */) {
       // viteVuePluginOptions: {},
 
       vitePlugins: [
-        [
-          '@intlify/vite-plugin-vue-i18n',
-          {
-            // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-            // compositionOnly: false,
-
-            // if you want to use named tokens in your Vue I18n messages, such as 'Hello {name}',
-            // you need to set `runtimeOnly: false`
-            // runtimeOnly: false,
-
-            // you need to set i18n resource including paths !
-            include: path.resolve(__dirname, './src/i18n/**')
-          }
-        ]
+        []
       ]
     },
 

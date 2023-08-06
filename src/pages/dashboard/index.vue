e473fd180b78e717a8b0015e6087a62b22fa6472
burcuicen <burcuicen2000@gmail.com>
2023-08-06 18:10:10 +0300

    feat:add welcome title to dashboard

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,9 +1,47 @@
 <template lang="pug">
-div Helloo
+.q-pa-md.hello
+  .hello__title
+    | Welcome &nbsp;
+    span.hello__subtitle {{ user.username }}
+  .hello__content
+    | This is your dashboard page
+    br
+    | Ready to start?
+
 </template>
 <script lang="ts">
+
+import { useAuthStore } from 'src/stores/auth';
 import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'DashboardPage',
+  computed: {
+    user() {
+      return useAuthStore().user;
+    },
+  },
 });
 </script>
+<style lang="scss" >
+.hello {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding-top: 100px;
+  font-size: 2rem;
+
+  &__subtitle {
+    font-weight: 700;
+    text-transform: capitalize;
+  }
+
+  &__content {
+    margin-top: 12px;
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+
+}
+</style>

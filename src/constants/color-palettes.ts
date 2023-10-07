2f3c201d240f6a84f9af4e3051719d3bd46c1576
burcuicen <burcuicen2000@gmail.com>
2023-10-07 18:59:11 +0300

    feat:add color palettes

---
+++ src/constants/color-palettes.ts
@@ -0,0 +1,2 @@
+export const COLOR_PALETTE_GENERAL = ['#57007B', '#AA0070', '#E0395C', '#FD7A4B', '#FFBA4B', '#F9F871']
+export const COLOR_PALETTE_PURPLE = ['#5f3a6e', '#8d6c9d', '#b99bcf', '#dbc0f2', '#f0e0fa']

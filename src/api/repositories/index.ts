9987cef3056f0cf9f64fffe00440a543bad0e503
burcuicen <burcuicen2000@gmail.com>
2023-10-15 02:04:35 +0300

    fix:remove unused files

--- src/api/repositories/index.ts
+++ src/api/repositories/index.ts
@@ -1,3 +1,2 @@
 export { AuthRepository } from './auth.repository'
 export { GoogleTrendsRepository } from './google-trends.repository'
-export { BarrierRepository } from './barrier.repository'
--- src/pages/dashboard/trends-over-time/index.vue
+++ src/pages/dashboard/trends-over-time/index.vue
@@ -1,19 +1,17 @@
 <template lang="pug">
 .q-pa-md.page.page--trends
   TInDevelopment
-
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import TInDevelopment from 'src/components/t-in-development/index.vue';
-
+import { defineComponent } from 'vue'
+import TInDevelopment from 'src/components/t-in-development/index.vue'
 export default defineComponent({
   name: 'TrendsOverTimePage',
   components: {
     TInDevelopment
   }
-});
+})
 </script>
 
 <style lang="scss"></style>

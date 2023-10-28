057d5846807bb8ef62d0dcf71e803e31c0c3eb4a
burcuicen <burcuicen2000@gmail.com>
2023-10-28 17:04:32 +0300

    feat:add topic tunes page

---
+++ src/pages/dashboard/topic-tunes/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.q-pa-md.page.page--regional
+    THeader(
+      title="Topic Tunes"
+      content="Enter a keyword to explore the category trends"
+    )
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import THeader from 'src/pages/dashboard/components/header.vue'
+
+export default defineComponent({
+  name: 'TopicTunesPage',
+  components: {
+    THeader
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
